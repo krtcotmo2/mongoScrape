83e221c572ae42d89b7214d227de1943f70e7c3b
Kurt Cooney <krtcotmo2@gmail.com>
2019-03-24 19:03:39 -0400

    Initial set up before beginning homework.

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers/router');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use('/', routes);
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 });
