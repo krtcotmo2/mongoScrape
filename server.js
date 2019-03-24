2b049acceec56b36d06b871e287ddffac941e79e
Kurt Cooney <krtcotmo2@gmail.com>
2019-03-24 16:45:28 -0400

    back to travis

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
-const express = require("express");
-let PORT;
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 PORT = process.env.PORT || 8080;
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.listen(PORT, function(){
+     console.log("listening on port", PORT)
+});
