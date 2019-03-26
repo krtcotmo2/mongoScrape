c2a392591afc991fb68974bb590f69e864ca4476
Kurt Cooney <krtcotmo2@gmail.com>
2019-03-25 23:22:09 -0400

    save methods - add some notes

--- controllers/router.js
+++ controllers/router.js
@@ -1,12 +1,15 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable arrow-body-style */
 /* eslint-disable prefer-const */
 const express = require('express');
 const axious = require('axios');
 const cheerio = require('cheerio');
+const db = require('../models');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Mogo Scraper' });
+  return res.render('index', { title: 'Mogo Scraper' });
 });
 
 router.get('/getArticles', (req, res) => {
@@ -24,10 +27,40 @@ router.get('/getArticles', (req, res) => {
       });
       return res.json({ articles: artArr });
     }).catch((err) => {
-      res.status(404).json({ message: `no data to return ${err}` });
+      return res.status(404).json({ message: `no data to return ${err}` });
+    });
+});
+router.get('/getSavedArticle', (req, res) => {
+  db.Article.find()
+    .populate('note')
+    .sort({ created: -1 })
+    .then((dbMessage) => {
+      return res.json(dbMessage);
+    });
+});
+router.post('/saveArticle', (req, res) => {
+  db.Article.create(req.body)
+    .then((dbMessage) => {
+      return res.json({ message: `Success ${dbMessage}` });
+    })
+    .catch((err) => {
+      res.json({ errorMessage: err });
+    });
+});
+router.post('/newComment', (req, res) => {
+  db.Note.create(req.body)
+    .then((noteResponse) => {
+      return noteResponse;
+    })
+    .then((noteResponse) => {
+      req.body.note = noteResponse._id;
+      db.Article.create(req.body)
+        .then((artData) => {
+          res.json(artData);
+        });
     });
 });
 router.get('*', (req, res) => {
-  res.redirect(req.url);
+  return res.redirect(req.url);
 });
 module.exports = router;
